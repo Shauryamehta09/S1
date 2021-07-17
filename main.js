d3b546526dc560549e200549325189c0a9587b4c
Shauryamehta09 <87532341+Shauryamehta09@users.noreply.github.com>
2021-07-17 10:36:36 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function changeimage() {
+	document.getElementById("IMG1").src = "https://th.bing.com/th/id/R.7b364d490710517adb4b5aa9e0067070?rik=cLkpH%2fPr1uM6zA&riu=http%3a%2f%2fwallup.net%2fwp-content%2fuploads%2f2015%2f12%2f236934-nature-landscape-sunrise-mountain-clouds-road-snowy_peak.jpg&ehk=xW24YIbQGye%2fIUYZ2E4AIHWRDtyNrtyyhTQfg0hPF1A%3d&risl=&pid=ImgRaw"
+}
